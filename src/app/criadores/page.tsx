e367ed85a0dced9dd514221993d059cb3f6dc289
Vinicius <viniciussantanna2005@gmail.com>
2024-11-01 20:46:14 -0300

    commit tudo - página criadores / ajustes / imagens / card

--- src/app/criadores/page.tsx
+++ src/app/criadores/page.tsx
@@ -1,12 +1,24 @@
-import Card from "@/components/Card/Card";
+import Vinicius from "@/assets/imagem.vinicius.jpg"
+import Felipe from "@/assets/imagem.felipafa.jpg"
+import Pedro from "@/assets/imagem.gemeo.jpg"
 import styles from "@/app/criadores/page.module.css"
+import { Card } from "@/components/Card/Card"
+import Link from "next/link"
 
 export default function Criadores(){
     return(
-        <main className={styles.main}>
-            <Card />
-            <Card />
-            <Card />
-        </main>
+        <>
+            <main className={styles.main}>
+                <Card imagem_criador={Felipe} nome={"Felipe Rosa Peres"} RM={"RM 557636"} turma={"1TDSPX"} />
+
+                <Card imagem_criador={Vinicius} nome={"Vinícius de Souza Sant Anna"} RM={"RM 556841"} turma={"1TDSPX"} />
+
+                <Card imagem_criador={Pedro} nome={"Pedro Henrique De Souza"} RM={"RM 555533"} turma={"1TDSPX"} />
+            </main>
+            
+            <div className={styles.ajuste_repo}>
+                <Link href="https://github.com/ViniciusSantanaa/challenge_4sprint.git" className={styles.repo}>Repositório</Link>
+            </div>
+        </>
     )
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,10 +2,20 @@ import Image from "next/image"
 import a from "@/assets/civic-site.png"
 import styles from "@/components/Card/Card.module.css"
 
-export default function Card(){
-    return(
-        <div className={styles.fundo_card}>
-
-        </div>
-    )
+interface LayoutCard {
+    nome?: string
+    RM?: string
+    turma?: string
+    imagem_criador: string
 }
+
+export const Card: React.FC<LayoutCard> = ({ nome, RM, turma, imagem_criador }) => {
+    return (
+        <div className={styles.fundo_card}>
+            <Image src={imagem_criador} alt={"Imagem do criador"} width={200} height={200} className={styles.imagem}/>
+            <p className={styles.descricao}>{nome}</p>
+            <p className={styles.descricao}>{RM}</p>
+            <p className={styles.descricao}>{turma}</p>
+        </div>
+    );
+};
