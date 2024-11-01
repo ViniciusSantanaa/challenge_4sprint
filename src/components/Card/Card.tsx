6ff41d4955d4dfbc821cf5c0ecb0ac9d288a8cc0
Vinicius <viniciussantanna2005@gmail.com>
2024-11-01 16:03:35 -0300

    Component card - em andamento

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+import a from "@/assets/civic-site.png"
+import styles from "@/components/Card/Card.module.css"
+
+export default function Card(){
+    return(
+        <div className={styles.fundo_card}>
+
+        </div>
+    )
+}
