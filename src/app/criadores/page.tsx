c7a088a4318fd2b4a83425ef3f4152ddd0d9aa87
Vinicius <viniciussantanna2005@gmail.com>
2024-11-01 16:02:51 -0300

    página criadores - em andamento

---
+++ src/app/criadores/page.tsx
@@ -0,0 +1,12 @@
+import Card from "@/components/Card/Card";
+import styles from "@/app/criadores/page.module.css"
+
+export default function Criadores(){
+    return(
+        <main className={styles.main}>
+            <Card />
+            <Card />
+            <Card />
+        </main>
+    )
+}
