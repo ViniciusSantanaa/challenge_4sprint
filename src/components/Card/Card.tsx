2506c3c51e0e4ff39da78ebc14f58d316b225d38
Vinicius <viniciussantanna2005@gmail.com>
2024-11-04 22:38:02 -0300

    muda a tipagem

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,17 @@
-import Image from "next/image"
 import styles from "@/components/Card/Card.module.css"
+import Image, { StaticImageData } from "next/image"
 
 interface LayoutCard {
     texto1?: string
     texto2?: string
     texto3?: string
-    imagemCriador: string
+    imagemCriador: StaticImageData
 }
 
 export const Card: React.FC<LayoutCard> = ({ texto1, texto2, texto3, imagemCriador }) => {
     return (
         <div className={styles.fundo_card}>
-            <Image src={imagemCriador} alt={"Imagem do criador"} width={200} height={200} className={styles.imagem}/>
+            <Image src={imagemCriador} alt={"Imagem do criador"} width={200} height={200} className={styles.imagem} />
             <p className={styles.descricao}>{texto1}</p>
             <p className={styles.descricao}>{texto2}</p>
             <p className={styles.descricao}>{texto3}</p>
